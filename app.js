0b141684dcc62fed1cfe6f1f5f8dca7446c27f65
rushikas569628 <reddyreducation@gmail.com>
2024-03-14 11:13:31 -0500

    new endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var endpoint = require('./routes/mydata');
 var app = express();
 
 // view engine setup
@@ -20,7 +20,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/mydata', endpoint);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
